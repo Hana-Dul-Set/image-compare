7a90b1cfc2f8c8c763885bd45b9bf32ba7ffd9a1
PROLCY <ckd248@naver.com>
2023-06-07 17:23:01 +0900

    write_data_utils.py 리팩토링

--- write_data_utils.py
+++ write_data_utils.py
@@ -14,6 +14,23 @@ def get_pixel_sum(pixel_list):
             sum += rgb
     return sum
 
+def set_up_csv_writer():
+    csv_file = open('image_pixel_data.csv', 'a')
+    field_name = ['img_filename', 'img_size', 'img_pixel_sum']
+    csv_writer = csv.DictWriter(csv_file, field_name)
+    csv_writer.writerow({ 'img_filename': 'img_filename', 'img_size': 'img_size', 'img_pixel_sum': 'img_pixel_sum'})
+    return csv_writer
+
+def get_row_data_from_image_file(IMAGE_DIR_PATH, img_filename):
+    img = Image.open(IMAGE_DIR_PATH + img_filename)
+    img_pixel_list = list(img.getdata())
+
+    img_size = len(img_pixel_list)
+    img_pixel_sum = get_pixel_sum(img_pixel_list)
+
+    row_data = { 'img_filename': img_filename, 'img_size': img_size, 'img_pixel_sum': img_pixel_sum}
+    return row_data
+
 def write_image_pixel_data(IMAGE_DIR_PATH):
 
     print("Writing Image Pixel Data on image_pixel_data.csv")
@@ -22,15 +39,7 @@ def write_image_pixel_data(IMAGE_DIR_PATH):
     if '.DS_Store' in img_filename_list:
         img_filename_list.remove('.DS_Store')
 
-    csv_file = open('image_pixel_data.csv', 'a')
-    field_name = ['img_filename', 'img_size', 'img_pixel_sum']
-    csv_writer = csv.DictWriter(csv_file, field_name)
-    csv_writer.writerow({ 'img_filename': 'img_filename', 'img_size': 'img_size', 'img_pixel_sum': 'img_pixel_sum'})
+    csv_writer = set_up_csv_writer()
 
     for img_filename in tqdm(img_filename_list):
-        img = Image.open(IMAGE_DIR_PATH + img_filename)
-        img_pixel_list = list(img.getdata())
-
-        img_size = len(img_pixel_list)
-        img_pixel_sum = get_pixel_sum(img_pixel_list)
-        csv_writer.writerow({ 'img_filename': img_filename, 'img_size': img_size, 'img_pixel_sum': img_pixel_sum})
+        csv_writer.writerow(get_row_data_from_image_file(IMAGE_DIR_PATH, img_filename))
