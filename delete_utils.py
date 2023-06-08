b8b490c87d0419daf2454a824f2ce992b9a21208
PROLCY <ckd248@naver.com>
2023-06-08 13:26:56 +0900

    add delete_utils

---
+++ delete_utils.py
@@ -0,0 +1,29 @@
+import os, shutil
+
+def get_duplicated_image_path_list(IMAGE_DIR_PATH):
+    duplicated_image_path_list = []
+    with open('image_same.csv', 'r') as csv_file:
+        image_pairs_list = csv_file.readlines()
+        for pair in image_pairs_list:
+            image_file_name = pair[:-1].split(',')[1]
+            file_path = IMAGE_DIR_PATH + image_file_name
+            duplicated_image_path_list.append(file_path)
+    return duplicated_image_path_list
+
+def delete_file(file_path):
+    if(os.path.exists(file_path)):
+        os.remove(file_path)
+        print(file_path + " is successfully removed!")
+    else:
+        print(file_path + " is not valid!")
+    return
+
+
+def delete_duplicated_images(IMAGE_DIR_PATH):
+    duplcated_image_path_list = get_duplicated_image_path_list(IMAGE_DIR_PATH)
+    if not os.path.isdir('./removed_image'):
+        os.mkdir('removed_image')
+    for image_path in duplcated_image_path_list:
+        shutil.copyfile(image_path, './removed_image/' + image_path.split('/')[-1])
+        delete_file(image_path)
+    return
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from write_data_utils import *
 from compare_utils import *
 
+from delete_utils import *
+import os
+
 """
 pip install pillow
 pip install tqdm
@@ -8,6 +11,12 @@ pip install tqdm
 
 ## 이미지 파일이 있는 경로 입력(맨 뒤에 '/' 필수 입력) ex) './image/'
 IMAGE_DIR_PATH = 'PATH'
+GET_DUPLICATED_IMAGE_PAIR_OPTION = False
+DELETE_OPTION = False
 
-write_image_pixel_data(IMAGE_DIR_PATH)
-write_same_image_pairs(IMAGE_DIR_PATH)
+if(GET_DUPLICATED_IMAGE_PAIR_OPTION):
+    write_image_pixel_data(IMAGE_DIR_PATH)
+    write_same_image_pairs(IMAGE_DIR_PATH)
+
+if(DELETE_OPTION):
+    delete_duplicated_images(IMAGE_DIR_PATH)
